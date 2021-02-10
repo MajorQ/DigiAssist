d41b9b4e5d75ee6742b4dc32093c3f9cc24fb9a7
Salman <mohammadalfarisi00@gmail.com>
2021-02-10 12:06:14 +0700

    webpack setup for version 2.0

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,17 @@
+import { browser } from "webextension-polyfill-ts"
+
 const search_button = document.getElementById('search_button');
 
-search_button.onclick = () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    var a = await browser.storage.local.get('unknown');
+
+    if (Object.keys(a).length == 0) {
+        console.log('it\'s empty');
+    }
+});
+
+search_button.addEventListener('click', () => {
     console.log('yes');
-}
+});
+
+
